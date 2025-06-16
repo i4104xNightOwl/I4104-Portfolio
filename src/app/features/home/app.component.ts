4c1b73b33d3887986dad8846e255451854381358
I4104 Night Owl <I4104.vn@gmail.com>
2025-06-16 18:48:41 +0700

    FEAT(Main Layout): Tạo main layout

---
+++ src/app/features/home/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+// import { HomeViewModel } from './app.viewmodel';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
