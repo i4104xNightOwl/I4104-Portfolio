6375c77ba40b493bc4de82c96b8b05abdcbc9139
I4104 Night Owl <I4104.vn@gmail.com>
2025-06-18 18:20:05 +0700

    FEAT(Navbar): Tạo thanh navbar

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [NzIconModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
