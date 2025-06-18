78e89500b4b0db0b58cc623367f0ec4a03852eb4
I4104 Night Owl <I4104.vn@gmail.com>
2025-06-18 19:32:03 +0700

    FEAT(Info): Tạo trang info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/app.component';
+import { InfoComponent } from './features/info/info.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'info', component: InfoComponent },
 ];
