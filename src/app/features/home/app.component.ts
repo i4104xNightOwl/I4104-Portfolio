7b3dd0673f20b0d5080be5d6a47e843ad034ad78
I4104 Night Owl <I4104.vn@gmail.com>
2025-06-20 21:09:14 +0700

    FEAT(Info): Tạo giao diện cho trang info

--- src/app/features/home/app.component.ts
+++ src/app/features/home/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import { HomeViewModel } from './app.viewmodel';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 
@@ -9,7 +10,7 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [NzGridModule, NzLayoutModule],
+  imports: [NzGridModule, NzLayoutModule, NzIconModule],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
--- src/app/features/info/info.component.ts
+++ src/app/features/info/info.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutComponent, NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-info',
-  imports: [],
+  imports: [NzLayoutModule, NzIconModule, NzGridModule],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })
